7a30d1296c5897d9a3a94ca33cbeadcf4d68b3b5
Łukasz Tshipenchko <dev@zxc.sx>
2025-01-22 02:08:54 +0500

    Formatted code

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
+from ch9329 import keyboard
+from ch9329 import mouse
 from ch9329.mouse import MouseCtrl
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
-
 from serial import Serial
-from ch9329 import keyboard
-from ch9329 import mouse
 
 serial = Serial("/dev/ttyUSB2", 9600, timeout=0.05)
 
@@ -17,7 +16,8 @@ app.mount("/", StaticFiles(directory="public", html=True), name="public")
 
 @api.post("/keydown")
 def keydown(key: str, control: bool = False, shift: bool = False, alt: bool = False):
-    key = {"meta": "win", "capslock": "caps_lock", "arrowright": "arrow_right", "arrowleft": "arrow_left", "arrowdown": "arrow_down", "arrowup": "arrow_up"}.get(key, key)
+    key = {"meta": "win", "capslock": "caps_lock", "arrowright": "arrow_right", "arrowleft": "arrow_left",
+           "arrowdown": "arrow_down", "arrowup": "arrow_up"}.get(key, key)
     if key in {"shift", "control", "alt"}: return
 
     if control and alt:
@@ -30,21 +30,27 @@ def keydown(key: str, control: bool = False, shift: bool = False, alt: bool = Fa
         modifiers = []
 
     keyboard.press(serial, key, modifiers)
+
+
 @api.post("/keyup")
 def keyup():
     keyboard.release(serial)
+
 
 @api.post("/mousedown")
 def mousedown(button: MouseCtrl):
     mouse.press(serial, button)
 
+
 @api.post("/mouseup")
 def mouseup():
     mouse.release(serial)
 
+
 @api.post("/mousemove")
 def mousemove(x: int, y: int):
     mouse.move(serial, x, y)
+
 
 if __name__ == '__main__':
     print("Execute `fastapi dev main.py` to start the server.")
